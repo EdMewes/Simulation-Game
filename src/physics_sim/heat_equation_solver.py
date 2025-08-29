0917c0b4dff32ac978f987bdc65fade3e2af099f
EdMewes <eddym386@gmail.com>
2025-08-29 14:38:41 +0100

    sparse and banded matrix solvers

--- src/physics_sim/heat_equation_solver.py
+++ src/physics_sim/heat_equation_solver.py
@@ -18,6 +18,12 @@ Equation:
         - Firedrake
         - Dedalus
 
+        
+
+    NOTES ON METHODS;
+        - Diffuesion - implicit
+        - Wave eq - explicit
+
 """
 
 import numpy as np
@@ -29,9 +35,38 @@ from physics_sim.timing import time_funtion as tf
 import timeit
 
 
+
+from sympy.solvers.pde import pdsolve
+from sympy import Function, Eq
+from sympy.abc import x, y
+
+
+
+test_size = 100
+run_cases = 2
 # @tf
 
-def CNM_matrix_build(x_size, y_size):
+
+def gaussian2d(shape=(100, 100), sigma=10.0, amplitude=1.0, normalize=False):
+    h, w = shape
+    y = np.arange(h) - (h - 1) / 2.0
+    x = np.arange(w) - (w - 1) / 2.0
+    X, Y = np.meshgrid(x, y)  # X varies along columns, Y along rows
+
+    g = amplitude * np.exp(-(X**2 + Y**2) / (2.0 * sigma**2))
+    if normalize:
+        s = g.sum()
+        if s != 0:
+            g = g / s
+    # print(np.shape(g))
+    return g
+
+
+# Example: 100x100, centered, sigma=12, normalized to sum to 1
+data = gaussian2d((100, 100), sigma=12.0, amplitude=1.0, normalize=True)
+
+# Banded matrix unusable at larger (>100x100 grid size due to memory requirements)
+def CNM_band_matrix_build(x_size, y_size):
     # Define size of simulation
     def_coef = 100
     delta_t = 0.05
@@ -57,31 +92,11 @@ def CNM_matrix_build(x_size, y_size):
     row = band_mat[x_size + 1]
     row.fill(-alpha)
     row[::x_size] = 0
-
     return band_mat
 
-
-def CNM_solve(initial_data, x_size, band_mat):
-    
-
-    # print(np.shape(band_mat))
-    # print(band_mat)
-    band_mat_next_step = solve_banded(
-        (x_size, x_size), band_mat, initial_data.ravel()
-    )
+def CNM_band_solve(initial_data, x_size, band_mat):
+    band_mat_next_step = solve_banded((x_size, x_size), band_mat, initial_data.ravel())
     return band_mat_next_step
-
-
-bmat = CNM_matrix_build(100,100)
-
-data = np.random.rand(100, 100)
-# time = timeit.timeit(lambda: CNM_spmatrix_build(100, 100, data), number=10)
-time = timeit.timeit(lambda: CNM_solve(data, 100, bmat), number=100)
-print("Average runtime of banded:", time/100)
-
-
-
-
 
 
 
@@ -96,39 +111,40 @@ def CNM_spmatrix_build(x_size, y_size):
 
     alpha = (delta_t * def_coef) / (2 * dx2)
 
-    # fill values +- x, edge cases accounted for
-    outer_sub_diag = -alpha*np.ones(x_size*y_size-x_size)
-    inner_sub_diag = -alpha*np.ones(x_size*y_size-1)
-    inner_sub_diag[x_size-1::x_size] = 0
-    main_diag = (1+4*alpha)*np.ones(x_size*y_size)
+    # fill values +- x
+    outer_sub_diag = -alpha * np.ones(x_size * y_size - x_size)
+    inner_sub_diag = -alpha * np.ones(x_size * y_size - 1)
+    inner_sub_diag[x_size - 1 :: x_size] = 0
+    main_diag = (1 + 4 * alpha) * np.ones(x_size * y_size)
 
     # initialise banded matrix
-    sparse_band_mat = diags([outer_sub_diag,inner_sub_diag,
-                             main_diag, inner_sub_diag, outer_sub_diag],
-                             [x_size, 1, 0, -1, -x_size], format='csc')
-
+    sparse_band_mat = diags(
+        [outer_sub_diag, inner_sub_diag, main_diag, inner_sub_diag, outer_sub_diag],
+        [x_size, 1, 0, -1, -x_size],
+        format="csc",
+    )
+    print("done")
     return sparse_band_mat
+
 
 def CNM_spsolve(sp_matrix, initial_data):
     # Solve sparse matrix
+    print("starting")
     band_mat_next_timestep = spsolve(sp_matrix, initial_data.ravel())
     return band_mat_next_timestep
 
-spmat = CNM_spmatrix_build(100,100)
 
-data = np.random.rand(100, 100)
-# time = timeit.timeit(lambda: CNM_spmatrix_build(100, 100, data), number=10)
-time = timeit.timeit(lambda: CNM_spsolve(spmat, data), number=100)
-print("Average runtime of sparse:", time/100)
-
+spmat = CNM_spmatrix_build(test_size, test_size)
+data = np.random.rand(test_size, test_size)
+time = timeit.timeit(lambda: CNM_spsolve(spmat, data), number=run_cases)
+print("Average runtime of sparse:", time / run_cases)
 
 
-
-
-
-
-def sympy_sovle():
-    return
+# Sympy cannot solve 2D PDE, but can use for notation
+# def sympy_build_eq():
+#         return
+# def sympy_sovle():
+#     return
 
 
 def scipy_sovle():
