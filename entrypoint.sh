3700aeea2a65056a4eef460ba596472189ed1f3e
EdMewes <eddym386@gmail.com>
2025-09-08 23:02:06 +0100

    improved

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ENV_FILE="/tmp/last_env"
+ENV_FILE="/app/.last_env"
 
 if [ -f "$ENV_FILE" ]; then
     LAST_ENV=$(cat "$ENV_FILE")
@@ -8,18 +8,20 @@ else
     LAST_ENV=""
 fi
 
-echo "Welcome to the simulation container!"
-echo "Available environments: dedalus-env (alias: d), fenics-env (alias: f)"
-
-read -p "Which environment would you like to use? [last: $LAST_ENV] " ENV_CHOICE
+if [ -t 0 ]; then
+    echo "Welcome to the simulation container!"
+    echo "Available environments: dedalus-env (alias: d), fenics-env (alias: f)"
+    read -p "Which environment would you like to use? [last: $LAST_ENV] " ENV_CHOICE
+else
+    ENV_CHOICE=""
+fi
 
 case "$ENV_CHOICE" in
     d) ENV_NAME="dedalus-env" ;;
     f) ENV_NAME="fenics-env" ;;
-    "") ENV_NAME="$LAST_ENV" ;;  # default to last used
+    "") ENV_NAME="$LAST_ENV" ;;
     *) ENV_NAME="$ENV_CHOICE" ;;
 esac
-
 
 if [ -z "$ENV_NAME" ]; then
     echo "No environment selected. Starting default bash."
