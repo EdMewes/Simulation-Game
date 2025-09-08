214143eecced3071c929e0a89fca79325bd8245a
EdMewes <eddym386@gmail.com>
2025-09-08 22:43:19 +0100

    Bash first attempt

---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ENV_FILE="/tmp/last_env"
+
+if [ -f "$ENV_FILE" ]; then
+    LAST_ENV=$(cat "$ENV_FILE")
+else
+    LAST_ENV=""
+fi
+
+echo "Welcome to the simulation container!"
+echo "Available environments: dedalus-env (alias: d), fenics-env (alias: f)"
+
+read -p "Which environment would you like to use? [last: $LAST_ENV] " ENV_CHOICE
+
+case "$ENV_CHOICE" in
+    d) ENV_NAME="dedalus-env" ;;
+    f) ENV_NAME="fenics-env" ;;
+    "") ENV_NAME="$LAST_ENV" ;;  # default to last used
+    *) ENV_NAME="$ENV_CHOICE" ;;
+esac
+
+
+if [ -z "$ENV_NAME" ]; then
+    echo "No environment selected. Starting default bash."
+    exec bash
+fi
+
+if conda info --envs | grep -xq -- "$ENV_NAME"; then
+    printf '%s\n' "$ENV_NAME" > "$ENV_FILE"
+    exec conda run -n "$ENV_NAME" bash
+else
+    echo "Environment '$ENV_NAME' not found. Starting default bash."
+    exec bash
+fi
